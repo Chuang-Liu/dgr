2d691423e315d75b6d6aa7042806a90a04d62c4d
lychur <lychur@users.noreply.github.com>
2015-07-08 23:01:44 +0800

    2nd stage not finished

--- dgr.cpp
+++ dgr.cpp
@@ -3,37 +3,44 @@
 #include <queue>
 
 struct node {
-	private:
-		int x, y;
-		int id, radius;
-		vector<node *> path;
-	public:
-		node(int xx, int yy, int a, int b):
-			x(xx), y(yy), id(a), radius(b)
-	{
-	}
+	int x, y;
+	int id, radius;
+	int[1600] next;
 }
 
-int centralized(set<node> d, priority_queue<node, vector<node>, cmp> q) {
-	while (!q.empty()) {
-		d.push(q.top());
-		u = q.top().id;
-		q.pop();
-		v = q.top().id;
-		while (map[u][v] && map[v][u]) {
-			q.pop();
+int centralized(set<int> ds, struct node[] q, int qsize) {
+	set<int>::iterator si;
+	int i, j, k;
+	bool flag[qsize];
+	
+	i = 0;
+	memset(flag, 0 ,sizeof(bool)*qsize);
+	while (i < qsize) {
+		ds.insert(i);
+		flag[i] = 1;
+		for (j=i+1; j<qsize; j++) {
+			if (map[i][j] && map[j][i]) flag[j] = 1;
+		}
+		while ((i<qsize) && (flag[i])) i+=1;
+	}
+
+	for (k=0; k<ds.size; k++) {
+		for (i=0; i<di; i++) {
+			for (j=0; j<di; j++) {
+				if (d[i][j] > (d[i][k]+d[k][j])) {
+					d[i][j] = d[i][k] + d[k][j];
+					q[ds[i]].next[ds[j]] = q[ds[i]].next[ds[k]];
+				}
+			}
 		}
 	}
-
 }
 
-void GenerateDiskGraph(struct node[] q, int width, int height) {
+void GenerateDiskGraph(struct node[] q, int m, int width, int height) {
 	int i, n;
-	node nd;
 
 	n = rand(m);
 	const int maxr = width*width + height*height;
-	q[0] = nd;
 	q[0].x = rand(width);
 	q[0].y = rand(height);
 	q[0].id = 0;
@@ -46,7 +53,7 @@ void GenerateDiskGraph(struct node[] q, int width, int height) {
 		q[i].id = i;
 		q[i].r = rand(maxr-minr) + minr;
 	}
-	minr = int((x - q[n].x)*(x - q[n].x) + (y - q[n].y)*(y - q[n].y)) + 1;
+	minr = (x - q[n].x)*(x - q[n].x) + (y - q[n].y)*(y - q[n].y);
 	q[0].r = rand(maxr - minr) + minr;
 	for (; i<m; i++) {
 		nd = new node(rand(width), rand(height), i, rand(maxr));
@@ -60,11 +67,14 @@ void GenerateDiskGraph(struct node[] q, int width, int height) {
 void GenerateGraph(struct node[] q, int m, char* map, int mapsize) {
 	int i, j;
 	
-	memset(map, 0, mapsize);
 	for (i=0; i<m; i++) {
 		for (j=0; j<m; j++) {
 			if ((q[i].x-q[j].x)*(q[i].x-q[j].x)+(q[i].y-q[j].y)*(q[i].y-q[j].y) < q[i].r) {
 				map[i][j] = 1;
+				q[i].next[j] = j;
+			} else {
+				map[i][j] = 0;
+				q[i].next[j] = -1;
 			}
 		}
 	}
